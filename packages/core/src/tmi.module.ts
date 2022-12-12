947ada70ce400af32ccd65b02ac16d665ff3c4f9
XxshiftxX <contact@dev-shift.me>
2022-12-12 19:55:25 +0900

    refactor : Add options as provider

--- packages/core/src/tmi.module.ts
+++ packages/core/src/tmi.module.ts
@@ -1,24 +1,21 @@
-import { DynamicModule, Module, Provider } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { Client, Options } from 'tmi.js';
+import { DISCORD_MODULE_OPTIONS } from './constants/tmi-module-option.constant';
 
 @Module({})
 export class TmiModule {
   static forRoot(options: Options): DynamicModule {
-    const TmiClientProvider: Provider = {
-      provide: Client,
-      useFactory: async () => {
-        const client = new Client(options);
-
-        await client.connect();
-
-        return client;
-      },
-    };
-
     return {
       module: TmiModule,
       providers: [
-        TmiClientProvider,
+        {
+          provide: DISCORD_MODULE_OPTIONS,
+          useValue: options,
+        },
+        {
+          provide: Client,
+          useValue: new Client(options),
+        },
       ],
     };
   }
