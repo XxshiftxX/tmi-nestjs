9458a7cc97c5457b92ff1831991cb9c2fb91657f
XxshiftxX <contact@dev-shift.me>
2022-12-15 18:25:15 +0900

    feat : 샘플 코드 추가

--- packages/sample/src/app.controller.ts
+++ packages/sample/src/app.controller.ts
@@ -1,11 +1,26 @@
 import { Controller } from '@nestjs/common';
-import { On } from '@tmi-nestjs/core';
-import { ChatUserstate } from 'tmi.js';
+import {
+  Message, On, OnCommand, Parameter, Rest, UserState,
+} from '@tmi-nestjs/core';
+import { ChatUserstate, Userstate } from 'tmi.js';
 
 @Controller()
 export class AppController {
   @On({ events: 'message' })
   async handler(channel: string, tags: ChatUserstate, message: string, self: boolean) {
     console.log(channel, message);
+  }
+
+  @OnCommand({ prefix: '!' })
+  async command(
+    @UserState() userstate: Userstate,
+    @Parameter() first: string,
+    @Parameter() second: string,
+    @Rest() rest: string,
+  ) {
+    console.log('username', userstate.username);
+    console.log('first', first);
+    console.log('second', second);
+    console.log('rest', rest);
   }
 }
