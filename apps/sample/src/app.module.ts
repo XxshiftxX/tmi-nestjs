104f89bc6588e9f6a10e74b0a8cb7148f6306d95
XxshiftxX <qjfrntop12@gmail.com>
2023-04-11 10:29:29 +0900

    add @nestjs/config

--- apps/sample/src/app.module.ts
+++ apps/sample/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { TmiModule } from "@tmi-nestjs/core";
 
 @Module({
   imports: [
-    TmiModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: '.env.local',
+    }),
+    TmiModule.forRoot({
+      identity: {
+        username: process.env.TMI_USERNAME,
+        password: process.env.TMI_PASSWORD,
+      },
+      channels: [process.env.TMI_CHANNEL],
+    }),
   ],
 })
 export class AppModule {}
